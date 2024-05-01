8a2fd4045d7cb69339e25b0c7d77917cefc6500e
Kim <153905107+FireFlyDeveloper@users.noreply.github.com>
2024-05-01 22:21:02 +0800

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
-string input = "radar";
-bool isPalindrome = true;
+string input = "radar";  // Word to check for palindrome
+bool isPalindrome = true; // Flag to indicate if the word is a palindrome
 
-char myStack[20];
-int top = -1;
+char myStack[20]; // Stack to store characters
+int top = -1;     // Variable to keep track of the top of the stack
 
+// Function to check if stack is empty
 bool isUnderflow() {
   if (top == -1) {
     return true;
@@ -14,8 +15,10 @@ bool isUnderflow() {
   return false;
 }
 
+// Function to push a character onto the stack
 void push(char val) { myStack[++top] = val; }
 
+// Function to pop a character from the stack
 char pop() {
   if (isUnderflow()) {
     return '\0';
@@ -23,9 +26,10 @@ char pop() {
   return myStack[top--];
 }
 
+// Function to check if the word is a palindrome
 void checkPalindrome() {
-  int len = input.length();
-  int i, mid = len / 2;
+  int len = input.length(); // Length of the word
+  int i, mid = len / 2;     // Index for iterating through the word and finding the midpoint
 
   // Push the first half of the characters onto the stack
   for (i = 0; i < mid; i++) {
@@ -41,7 +45,7 @@ void checkPalindrome() {
   // Compare the remaining characters with the characters popped from the stack
   while (input[i] != '\0') {
     if (input[i] != pop()) {
-      isPalindrome = false;
+      isPalindrome = false; // If characters don't match, set palindrome flag to false
       break;
     }
     i++;
@@ -49,8 +53,9 @@ void checkPalindrome() {
 }
 
 int main() {
-  checkPalindrome();
+  checkPalindrome(); // Check if the word is a palindrome
 
+  // Output the result based on whether the word is a palindrome or not
   if (isPalindrome) {
     cout << "The word is a palindrome." << endl;
   } else {
